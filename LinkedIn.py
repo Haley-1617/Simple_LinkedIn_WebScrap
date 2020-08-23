7abf515526407f9b771c1df55deba80ffd8ae392
Haley-1617 <alicelai2920@gmail.com>
2020-08-23 08:49:42 -0700

    Allow user inputs job type, job location, and number of jobs they want to get

--- LinkedIn.py
+++ LinkedIn.py
@@ -4,28 +4,30 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from openpyxl import Workbook
-import sys, time
+import sys, time, math
 
 # setting language to en as default
 option = webdriver.ChromeOptions()
-option = webdriver.ChromeOptions()
 option.add_argument("--lang=en-US")
 driver = webdriver.Chrome(chrome_options=option)
-driver = webdriver.Chrome()
+# driver = webdriver.Chrome()
 
-# Search Software Engineer in California
-driver.get(
-    'https://www.linkedin.com/jobs/search/?keywords=Software%20Engineer&location=California%2C%20United%20States'
-)
 wait = WebDriverWait(driver, 10)
+# converting inputs to url form
+jobType = sys.argv[1].strip().replace(' ', '%20')
+location = sys.argv[2].strip().replace(' ', '%20').replace(',', '%2C')
+# Start searching
+driver.get('https://www.linkedin.com/jobs/search/?keywords=' + jobType +
+           '&location=' + location)
+
 # Sign in
 wait.until(EC.element_to_be_clickable((By.LINK_TEXT, "Sign in")))
 driver.find_element_by_link_text("Sign in").click()
 time.sleep(1)
 usrname = driver.find_element_by_id("username")
-usrname.send_keys(sys.argv[1])
+usrname.send_keys(sys.argv[4])
 pwd = driver.find_element_by_id("password")
-pwd.send_keys(sys.argv[2])
+pwd.send_keys(sys.argv[5])
 driver.find_element_by_tag_name('button').click()
 # driver.find_element_by_class_name('msg-overlay-bubble-header__button').click()
 
@@ -36,18 +38,28 @@ ws = wb.active
 titles = ['Job Title', 'Company', 'Location', 'Content']
 ws.append(titles)
 
-jobContainer = driver.find_elements_by_class_name('job-card-container')
-while len(jobContainer) < 25:
-    driver.execute_script("arguments[0].scrollIntoView();", jobContainer[-1])
+pageIdx = 1
+while pageIdx <= math.ceil(int(sys.argv[3]) / 25):
+    driver.Manage().Timeouts().ImplicitWait
     jobContainer = driver.find_elements_by_class_name('job-card-container')
-
-for job in jobContainer:
-    job.click()
-    card = list(job.text.split('\n'))
-    info = [card[0]]
-    info += card[1:3] if card[1] != " Promoted" else card[2:4]
-    content = driver.find_element_by_class_name('jobs-description__container')
-    info += [content.find_element_by_tag_name('span').text]
-    ws.append(info)
+    while len(jobContainer) < 25:
+        driver.execute_script("arguments[0].scrollIntoView();",
+                              jobContainer[-1])
+        jobContainer = driver.find_elements_by_class_name('job-card-container')
+    # TODO wait until job description has appeared in each job
+    for job in jobContainer:
+        job.click()
+        card = list(job.text.split('\n'))
+        info = [card[0]]
+        info += card[1:3] if card[1] != " Promoted" else card[2:4]
+        content = driver.find_element_by_class_name(
+            'jobs-description__container')
+        info += [content.find_element_by_tag_name('span').text]
+        ws.append(info)
+    pagination = driver.find_element(By.XPATH,
+                                     "//section[@aria-label='pagination']")
+    pageIdx += 1
+    pagination.find_element(By.XPATH, "//button[@aria-label='Page %d']" %
+                            pageIdx).click()
 wb.save('LinkedIn.xlsx')
 driver.close()
