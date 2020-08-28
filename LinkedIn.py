79a4f1b32f999fcc68c7e9bdda34995e00e5137d
Haley-1617 <alicelai2920@gmail.com>
2020-08-27 19:40:00 -0700

    Fixed job container click

--- LinkedIn.py
+++ LinkedIn.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from openpyxl import Workbook
 import sys, time, math
 
+# input: job position, job location, number of results, LinkedIn account username, LinkedIn account password
 # setting language to en as default
 option = webdriver.ChromeOptions()
 option.add_argument("--lang=en-US")
@@ -38,24 +39,33 @@ ws = wb.active
 titles = ['Job Title', 'Company', 'Location', 'Content']
 ws.append(titles)
 
+jobCount = 0
 pageIdx = 1
-while pageIdx <= math.ceil(int(sys.argv[3]) / 25):
-    driver.Manage().Timeouts().ImplicitWait
-    jobContainer = driver.find_elements_by_class_name('job-card-container')
+while jobCount < int(sys.argv[3]):
+    jobContainer = wait.until(
+        EC.presence_of_all_elements_located(
+            (By.CLASS_NAME, "job-card-container")))
+    # jobContainer = driver.find_elements_by_class_name('job-card-container')
     while len(jobContainer) < 25:
+        # TODO: sometimes this line is not working
         driver.execute_script("arguments[0].scrollIntoView();",
                               jobContainer[-1])
         jobContainer = driver.find_elements_by_class_name('job-card-container')
-    # TODO wait until job description has appeared in each job
     for job in jobContainer:
-        job.click()
+        # TODO: need to fix the click position, sometimes it'll direct to company's website
+        job.find_element_by_class_name("job-card-list__title").click()
         card = list(job.text.split('\n'))
         info = [card[0]]
         info += card[1:3] if card[1] != " Promoted" else card[2:4]
         content = driver.find_element_by_class_name(
             'jobs-description__container')
+        # TODO: some jobs' decription cannot be found in 'span' tage name
+        wait.until(EC.visibility_of(
+            (content.find_element_by_tag_name('span'))))
         info += [content.find_element_by_tag_name('span').text]
         ws.append(info)
+        jobCount += 1
+        if jobCount == int(sys.argv[3]): break
     pagination = driver.find_element(By.XPATH,
                                      "//section[@aria-label='pagination']")
     pageIdx += 1
